501d4dc61e55ecd037d054349f9efa511ceccccc
MNoble2014 <michealsimpson2013@gmail.com>
2021-11-30 16:50:22 -0500

    mouseleave first event listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './less/index.less'
 
 // Your code goes here!
+// 1 mouseleave
+const destination = document.querySelectorAll(".destination");
+destination.forEach(function(e) {
+    e.addEventListener('mouseleave', function() {
+        e.style.backgroundColor = 'purple';
+        e.style.color = 'white';
+    });
+})
